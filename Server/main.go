537f0460df445df42268226feb27373be1b01b6f
AravindRavikumar <akgr421@gmail.com>
2020-05-10 12:37:16 +0530

    Testing github.com/unrolled/render

--- Server/main.go
+++ Server/main.go
@@ -1,24 +1,32 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "github.com/gorilla/mux"
+    "html/template"
+    "github.com/unrolled/render"
+)
+ 
+func main() {
+    renderObj := render.New()
+    router := mux.NewRouter()
+    
+    router.HandleFunc("/catalogue", CatalogueHandler)
+    router.HandleFunc("/home", HomeHandler)
+    http.ListenAndServe(":3000", router)
+}
+ 
+func CatalogueHandler(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    title := vars["title"]
+    page := vars["page"]
 
-func intSeq() func() int {
-    i := 0
-    return func() int {
-        i++
-        return i
-    }
+    fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
 }
 
-func main() {
-
-    nextInt := intSeq()
-
-    fmt.Println(nextInt())
-    fmt.Println(nextInt())
-    fmt.Println(nextInt())
-
-    newInts := intSeq()
-    fmt.Println(newInts())
-    fmt.Println(nextInt())
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    
 }
